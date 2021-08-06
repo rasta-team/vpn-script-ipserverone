62619235be3c3bed49c9447d830478cc2f614ac6
akiraafudo <69959725+akiraafudo@users.noreply.github.com>
2021-08-06 11:21:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,8 +52,8 @@ echo "--------------------------------------------------------------------------
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 992"  | tee -a log-install.txt
+echo "   - Stunnel4                : 445"  | tee -a log-install.txt
 echo "   - Dropbear                : 442, 777"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080, 8888 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
