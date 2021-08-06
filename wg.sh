d3b29aa6671217e9c4379cba324c62aa0325616e
akiraafudo <69959725+akiraafudo@users.noreply.github.com>
2021-08-06 11:20:50 +0800

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -89,10 +89,10 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O add-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script/master/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script/master/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script/master/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script/master/renew-wg.sh"
+wget -O add-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script-ipserverone/main/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script-ipserverone/main/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script-ipserverone/main/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/akiraafudo/vpn-script-ipserverone/main/renew-wg.sh"
 chmod +x add-wg
 chmod +x del-wg
 chmod +x cek-wg
