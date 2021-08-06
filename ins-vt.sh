f2aaece7f1fe8e2bc98c22ff7fbe78dc4f334b1d
akiraafudo <69959725+akiraafudo@users.noreply.github.com>
2021-08-06 16:07:46 +0800

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -16,7 +16,7 @@ date
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 # install v2ray
-wget https://raw.githubusercontent.com/zahwanugrah/auto/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/akiraafudo/vpn-script-ipserverone/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
